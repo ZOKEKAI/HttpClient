ed3c1967b34e5f57bf835249678e245453221bc4
ZOKEKAI <zokekai@gmail.com>
2017-11-13 00:27:20 +0800

    uodate APIService.java

--- APIService.java
+++ APIService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -20,16 +19,10 @@ import org.apache.http.util.EntityUtils;
 
 /**
  * 提供给别的系统使用的业务接口
- * @title
- * @description
- * @author ljh 
- * @version 1.0
  */
 public class APIService {
-
 	
 	//带参数的get请求
-	
 	public HttpResult doGet(String url,Map<String, Object> map) throws Exception{
 		//封装httpclient 发送请求 处理业务 得到数据响应  httpresult
 		
@@ -47,10 +40,7 @@ public class APIService {
 			}
 		}
 		//2.创建httpget请求  
-		
 		HttpGet get = new HttpGet(builder.build());
-		
-		
 		
 		//4.执行请求   得到响应结果
 		CloseableHttpResponse response = httpClient.execute(get);
@@ -66,29 +56,23 @@ public class APIService {
 		HttpResult result = new HttpResult(code, body);
 		
 		//5.解析成httpresult
-		return result;
-		
-		
-		
+		return result;	
 	}
 	
 	//不带参数的get请求
-	
 	public HttpResult doGet(String url) throws Exception{
 		return doGet(url, null);
 	}
 	
 	//带参数的Post请求
-	
-	
 	public HttpResult doPost(String url,Map<String, Object> map) throws Exception{
 		//1.创建httpclient
 		CloseableHttpClient httpClient = HttpClients.createDefault();
+		
 		//2.创建httppost请求
 		HttpPost post =new HttpPost(url);
+		
 		//3.为httppost请求设置表单实体
-		
-		
 		if(map!=null){
 			Set<Entry<String,Object>> entrySet = map.entrySet();
 			
@@ -103,8 +87,8 @@ public class APIService {
 		}
 		
 		//4.执行请求
-		
 		CloseableHttpResponse response = httpClient.execute(post);
+		
 		//5.解析成httpresult
 		int code = response.getStatusLine().getStatusCode();
 		
@@ -116,15 +100,11 @@ public class APIService {
 		
 		HttpResult result = new HttpResult(code, body);
 		
-		return result;
-		
+		return result;	
 	}
-	
 	
 	//不带参数的post请求
 	public HttpResult doPost(String url) throws Exception{
 		return doPost(url, null);
 	}
-	
-	
 }
